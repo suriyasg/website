b5ace20e3fee8637ced43159f4f3381221d70f57
Sahithyan <sahithyan2701@gmail.com>
2023-11-01 17:39:13 +0530

    disable tailwindcss preflight

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	corePlugins: {
+		preflight: false
+	},
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
