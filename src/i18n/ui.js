075e3ac5adfe277275cd0201ced48dff45602d81
Sahithyan <sahithyan2701@gmail.com>
2023-11-10 12:36:16 +0530

    add (partial) tamil translations

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -30,6 +30,9 @@ export const defaultLang = "en";
  * @prop {string} nav__exam_centres
  * @prop {string} nav__past_papers
  * @prop {string} nav__about
+ * @prop {string} hero__title
+ * @prop {string} find_your_examination_centres__tile
+ * @prop {string} looking_for_past_papers__title
  */
 
 /**
@@ -41,11 +44,19 @@ export const ui = Object.freeze({
 		nav__exam_centres: "Exam Centres",
 		nav__past_papers: "Past Papers",
 		nav__about: "About Us",
+		hero__title: "Sri Lanka's biggest pioneer exams for G.C.E. (A/L) students",
+		find_your_examination_centres__tile: "Find your examination centres",
+		looking_for_past_papers__title: "Looking for Mora Exam Past Papers?",
 	},
 	ta: {
 		nav__timetable: "பரீட்சை அட்டவணை",
 		nav__exam_centres: "பரீட்சை நிலையங்கள்",
 		nav__past_papers: "கடந்த கால வினாத்தாள்கள்",
 		nav__about: "எம்மைப் பற்றி",
+		hero__title:
+			"க. பொ. த. (உ/த) மாணவர்களுக்கான இலங்கையின்  மிகப்பெரிய முன்னோடி பரீட்ச்சை",
+		find_your_examination_centres__tile:
+			"உங்களது பரீட்சை நிலையத்தை கண்டறியுங்கள்",
+		looking_for_past_papers__title: "கடந்த கால வினாத்தாள்களை தேடுகின்றீர்களா?",
 	},
 });
