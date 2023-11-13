bc376414813490e1e6fe8516ed0619b8e1bf90f2
Sahithyan <sahithyan2701@gmail.com>
2023-11-13 19:56:36 +0530

    add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import { defaultLang } from "./src/i18n/ui";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	redirects: {
-		"/": `/${defaultLang}/`,
-	},
+  integrations: [tailwind(), react()],
+  redirects: {
+    "/": `/${defaultLang}/`
+  }
 });
