0ecb5e7f61931a96be4f61e2e039c70593817de6
Sahithyan <sahithyan2701@gmail.com>
2023-11-16 09:57:06 +0530

    exam centres information updated

--- src/components/react/ExamCentreDetailsTable.jsx
+++ src/components/react/ExamCentreDetailsTable.jsx
@@ -1,21 +1,53 @@
-export default function ExamCentreDetailsTable({ data, languagePreference }) {
+/**
+ * @typedef Props
+ * @prop {typeof import("../../EXAM_CENTRES.json")["districts"][number]} data
+ * @prop {"ta" | "en"} languagePreference
+ */
+
+/**
+ * @param {Props} props
+ */
+export function ExamCentreDetailsTable(props) {
+	const { data, languagePreference } = props;
 	const columns = ["Center Name (English)", "Center Name (Tamil)"];
 	const langKey = languagePreference === "ta" ? "name_ta" : "name_en";
+	console.log(data);
+
+	const areCoordinatorsExamCentreSpecific =
+		typeof data.coordinators == "undefined";
 
 	return (
 		<div className="card">
 			<div className="header">
 				<h3>{data[langKey]}</h3>
-				<p>Coordinator: {data.coordinator}</p>
-				<p>Contact Number: {data.contact_number}</p>
+				{areCoordinatorsExamCentreSpecific ? null : (
+					<p>
+						{data.coordinators.length == 1 ? "Coordinator" : "Coordinators"}:{" "}
+						{data.coordinators.map((coordinator) => {
+							return (
+								<span key={coordinator.name_en}>
+									<b>{coordinator.name_en}</b> ({coordinator.contact_number}),{" "}
+								</span>
+							);
+						})}
+					</p>
+				)}
 			</div>
 			<div className="centers">
 				<h4>Centers</h4>
-				{data.centers.map((center, index) => (
-					<div key={index} className="center">
-						{center[langKey]}
-					</div>
-				))}
+				<ul>
+					{data.centers.map((center, index) => (
+						<li key={center.name_en} className="center">
+							{center[langKey]}{" "}
+							{!areCoordinatorsExamCentreSpecific ? null : (
+								<>
+									- {center.coordinator.name_en} (
+									{center.coordinator.contact_number})
+								</>
+							)}
+						</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	);
