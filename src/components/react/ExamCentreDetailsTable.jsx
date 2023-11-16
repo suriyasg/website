eb9fba0dc604ba86f7a7383c919d98c07c9a40fc
Sahithyan <sahithyan2701@gmail.com>
2023-11-16 15:01:19 +0530

    find your examination centres section updated

--- src/components/react/ExamCentreDetailsTable.jsx
+++ src/components/react/ExamCentreDetailsTable.jsx
@@ -8,47 +8,77 @@
  * @param {Props} props
  */
 export function ExamCentreDetailsTable(props) {
-	const { data, languagePreference } = props;
+	const { data: districtInfo, languagePreference } = props;
 	const columns = ["Center Name (English)", "Center Name (Tamil)"];
 	const langKey = languagePreference === "ta" ? "name_ta" : "name_en";
-	console.log(data);
+	console.log(districtInfo);
 
 	const areCoordinatorsExamCentreSpecific =
-		typeof data.coordinators == "undefined";
+		typeof districtInfo.coordinators == "undefined";
 
 	return (
-		<div className="card">
-			<div className="header">
-				<h3>{data[langKey]}</h3>
-				{areCoordinatorsExamCentreSpecific ? null : (
-					<p>
-						{data.coordinators.length == 1 ? "Coordinator" : "Coordinators"}:{" "}
-						{data.coordinators.map((coordinator) => {
-							return (
-								<span key={coordinator.name_en}>
-									<b>{coordinator.name_en}</b> ({coordinator.contact_number}),{" "}
-								</span>
-							);
-						})}
-					</p>
-				)}
+		<div>
+			<div>
+				<h3 className="text-4xl mb-4">{districtInfo[langKey]}</h3>
+				<p>
+					{areCoordinatorsExamCentreSpecific ? (
+						"Exam coordinators are specific to the exam centres."
+					) : (
+						<>
+							{districtInfo.coordinators.length == 1
+								? "District Coordinator"
+								: "District Coordinators"}
+							<br />
+							{districtInfo.coordinators.map((coordinator, i) => {
+								return (
+									<span key={coordinator.name_en}>
+										<b>{coordinator.name_en}</b> ({coordinator.contact_number})
+										{i == districtInfo.coordinators.length - 1 ? null : ", "}
+									</span>
+								);
+							})}
+						</>
+					)}
+				</p>
 			</div>
-			<div className="centers">
-				<h4>Centers</h4>
-				<ul>
-					{data.centers.map((center, index) => (
-						<li key={center.name_en} className="center">
-							{center[langKey]}{" "}
-							{!areCoordinatorsExamCentreSpecific ? null : (
-								<>
-									- {center.coordinator.name_en} (
-									{center.coordinator.contact_number})
-								</>
-							)}
-						</li>
+			<div className="mt-5">
+				<ul className="list-none m-0 p-0 grid gap-y-4 gap-x-2 grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))]">
+					{districtInfo.centers.map((centre, index) => (
+						<ExamCentreDetails
+							centre={centre}
+							languagePreference={languagePreference}
+						/>
 					))}
 				</ul>
 			</div>
 		</div>
+	);
+}
+
+/**
+ * @typedef ExamCentreDetails_Props
+ * @prop {typeof import("../../EXAM_CENTRES.json")["districts"][number]["centers"][number]} centre
+ * @prop {"ta" | "en"} languagePreference
+ */
+
+/**
+ * @param {ExamCentreDetails_Props} props
+ */
+function ExamCentreDetails(props) {
+	const { centre, languagePreference } = props;
+	const langKey = languagePreference === "ta" ? "name_ta" : "name_en";
+
+	return (
+		<li key={centre.name_en} className="w-fit">
+			<h4 className="my-0 text-xl">{centre[langKey]}</h4>
+			{/* @ts-ignore */}
+			{typeof centre.coordinator == "undefined" ? null : (
+				<p className="mt-2">
+					{/* @ts-ignore */}
+					<b>{centre.coordinator.name_en}</b> (
+					{centre.coordinator.contact_number})
+				</p>
+			)}
+		</li>
 	);
 }
