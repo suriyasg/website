dc7b4cc058598443fdfb100a968458298b7b9edc
Sahithyan <sahithyan2701@gmail.com>
2023-12-15 12:58:00 +0530

    use turso db in find-index-no page

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,7 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const applicants = sqliteTable("tbl_students", {
 	id: text("index_no", { length: 50, mode: "text" }).notNull(),
 	name: text("name", { length: 255, mode: "text" }),
-	email: text("email", { length: 255, mode: "text" }),
+	// email: text("email", { length: 255, mode: "text" }),
 	nic: text("nic", { length: 15, mode: "text" }).notNull(),
 });
 
